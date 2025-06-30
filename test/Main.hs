8ea30f99c40200452ef6a3dae890ffc7e4d30428
David Sánchez <davidslt+git@pm.me>
2025-06-30 22:47:31 +0100

    chore: setting up everything
    deleted old module, reusing expression types

--- test/Main.hs
+++ test/Main.hs
@@ -1,11 +1,16 @@
 module Main (main) where
 
-import RepresentingCode (prettyPrintExprUnitTests)
+-- import RepresentingCode (prettyPrintExprUnitTests)
 import Scanner.Props (scannerProperties)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 
 tests :: TestTree
-tests = testGroup "Lox Tests" [scannerProperties, prettyPrintExprUnitTests]
+tests =
+  testGroup
+    "Lox Tests"
+    [ scannerProperties
+    -- , prettyPrintExprUnitTests
+    ]
 
 main :: IO ()
 main = defaultMain tests

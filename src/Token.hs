89898af5790f9c62a66c27a3c3da2c5bf14276f1
David Sánchez <davidslt+git@pm.me>
2025-09-11 15:23:54 +0100

    test: hox is able to run all test suites
    previously only the last test was able to run, now I can run all

--- src/Token.hs
+++ src/Token.hs
@@ -1,7 +1,7 @@
 module Token
   ( Token (..),
     TokenType (..),
-    prettyPrint,
+    prettyPrintToken,
     isNumber,
     isString,
   )
@@ -70,47 +70,47 @@ data Token = Token
   deriving stock (Show, Eq)
 
 -- |  Pretty print a token the way the book expects
--- >>> prettyPrint (Token (IDENTIFIER "foo") 1)
+-- >>> prettyPrintToken (Token (IDENTIFIER "foo") 1)
 -- "IDENTIFIER foo null"
--- >>> prettyPrint (Token (NUMBER "42" 42.0) 1)
+-- >>> prettyPrintToken (Token (NUMBER "42" 42.0) 1)
 -- "NUMBER 42 42.0"
-prettyPrint :: Token -> String
-prettyPrint (Token (IDENTIFIER s) _) = "IDENTIFIER " <> s <> " null"
-prettyPrint (Token (STRING l s) _) = "STRING " <> l <> " " <> s
-prettyPrint (Token (NUMBER l n) _) = "NUMBER " <> l <> " " <> show n
-prettyPrint (Token LEFT_PAREN _) = "LEFT_PAREN ( null"
-prettyPrint (Token RIGHT_PAREN _) = "RIGHT_PAREN ) null"
-prettyPrint (Token LEFT_BRACE _) = "LEFT_BRACE { null"
-prettyPrint (Token RIGHT_BRACE _) = "RIGHT_BRACE } null"
-prettyPrint (Token COMMA _) = "COMMA , null"
-prettyPrint (Token DOT _) = "DOT . null"
-prettyPrint (Token MINUS _) = "MINUS - null"
-prettyPrint (Token PLUS _) = "PLUS + null"
-prettyPrint (Token SEMICOLON _) = "SEMICOLON ; null"
-prettyPrint (Token SLASH _) = "SLASH / null"
-prettyPrint (Token STAR _) = "STAR * null"
-prettyPrint (Token BANG _) = "BANG ! null"
-prettyPrint (Token BANG_EQUAL _) = "BANG_EQUAL != null"
-prettyPrint (Token EQUAL _) = "EQUAL = null"
-prettyPrint (Token EQUAL_EQUAL _) = "EQUAL_EQUAL == null"
-prettyPrint (Token GREATER _) = "GREATER > null"
-prettyPrint (Token GREATER_EQUAL _) = "GREATER_EQUAL >= null"
-prettyPrint (Token LESS _) = "LESS < null"
-prettyPrint (Token LESS_EQUAL _) = "LESS_EQUAL <= null"
-prettyPrint (Token AND _) = "AND and null"
-prettyPrint (Token CLASS _) = "CLASS class null"
-prettyPrint (Token ELSE _) = "ELSE else null"
-prettyPrint (Token FALSE _) = "FALSE false null"
-prettyPrint (Token FUN _) = "FUN fun null"
-prettyPrint (Token FOR _) = "FOR for null"
-prettyPrint (Token IF _) = "IF if null"
-prettyPrint (Token NIL _) = "NIL nil null"
-prettyPrint (Token OR _) = "OR or null"
-prettyPrint (Token PRINT _) = "PRINT print null"
-prettyPrint (Token RETURN _) = "RETURN return null"
-prettyPrint (Token SUPER _) = "SUPER super null"
-prettyPrint (Token THIS _) = "THIS this null"
-prettyPrint (Token TRUE _) = "TRUE true null"
-prettyPrint (Token VAR _) = "VAR var null"
-prettyPrint (Token WHILE _) = "WHILE while null"
-prettyPrint (Token EOF _) = "EOF  null"
+prettyPrintToken :: Token -> String
+prettyPrintToken (Token (IDENTIFIER s) _) = "IDENTIFIER " <> s <> " null"
+prettyPrintToken (Token (STRING l s) _) = "STRING " <> l <> " " <> s
+prettyPrintToken (Token (NUMBER l n) _) = "NUMBER " <> l <> " " <> show n
+prettyPrintToken (Token LEFT_PAREN _) = "LEFT_PAREN ( null"
+prettyPrintToken (Token RIGHT_PAREN _) = "RIGHT_PAREN ) null"
+prettyPrintToken (Token LEFT_BRACE _) = "LEFT_BRACE { null"
+prettyPrintToken (Token RIGHT_BRACE _) = "RIGHT_BRACE } null"
+prettyPrintToken (Token COMMA _) = "COMMA , null"
+prettyPrintToken (Token DOT _) = "DOT . null"
+prettyPrintToken (Token MINUS _) = "MINUS - null"
+prettyPrintToken (Token PLUS _) = "PLUS + null"
+prettyPrintToken (Token SEMICOLON _) = "SEMICOLON ; null"
+prettyPrintToken (Token SLASH _) = "SLASH / null"
+prettyPrintToken (Token STAR _) = "STAR * null"
+prettyPrintToken (Token BANG _) = "BANG ! null"
+prettyPrintToken (Token BANG_EQUAL _) = "BANG_EQUAL != null"
+prettyPrintToken (Token EQUAL _) = "EQUAL = null"
+prettyPrintToken (Token EQUAL_EQUAL _) = "EQUAL_EQUAL == null"
+prettyPrintToken (Token GREATER _) = "GREATER > null"
+prettyPrintToken (Token GREATER_EQUAL _) = "GREATER_EQUAL >= null"
+prettyPrintToken (Token LESS _) = "LESS < null"
+prettyPrintToken (Token LESS_EQUAL _) = "LESS_EQUAL <= null"
+prettyPrintToken (Token AND _) = "AND and null"
+prettyPrintToken (Token CLASS _) = "CLASS class null"
+prettyPrintToken (Token ELSE _) = "ELSE else null"
+prettyPrintToken (Token FALSE _) = "FALSE false null"
+prettyPrintToken (Token FUN _) = "FUN fun null"
+prettyPrintToken (Token FOR _) = "FOR for null"
+prettyPrintToken (Token IF _) = "IF if null"
+prettyPrintToken (Token NIL _) = "NIL nil null"
+prettyPrintToken (Token OR _) = "OR or null"
+prettyPrintToken (Token PRINT _) = "PRINT print null"
+prettyPrintToken (Token RETURN _) = "RETURN return null"
+prettyPrintToken (Token SUPER _) = "SUPER super null"
+prettyPrintToken (Token THIS _) = "THIS this null"
+prettyPrintToken (Token TRUE _) = "TRUE true null"
+prettyPrintToken (Token VAR _) = "VAR var null"
+prettyPrintToken (Token WHILE _) = "WHILE while null"
+prettyPrintToken (Token EOF _) = "EOF  null"

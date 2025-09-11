ed3858958e1511793775301ea521985986b31682
David Sánchez <davidslt+git@pm.me>
2025-09-11 16:36:31 +0100

    feat: current impl alias
    for the sake of simplicity?

--- app/Main.hs
+++ app/Main.hs
@@ -20,8 +20,8 @@ main = do
     ["--chap04_scanning", script] -> readFile' script >>= handleChap04Out . runChapter04
     ["--chap06_parsing", script] -> readFile' script >>= handleChap06Out . (runChapter04 >=> runChapter06)
     ["--chap07_evaluating", script] -> readFile' script >>= handleChap07Out . (runChapter04 >=> runChapter06 >=> runChapter07)
-    ["--chap08_statements", script] -> readFile' script >>= handleChap07Out . (runChapter04 >=> runChapter06 >=> runChapter07) -- TODO same as chapter 07 for now
-    [script] -> readFile' script >>= handleChap07Out . (runChapter04 >=> runChapter06 >=> runChapter07)
+    ["--chap08_statements", script] -> readFile' script >>= handleChap07Out . currentImpl -- TODO same as chapter 07 for now
+    [script] -> readFile' script >>= handleChap07Out . currentImpl
     _ -> do
       putStrLn "Usage: hox [[--<CHAP>] script]"
       exitWith (ExitFailure 64)
@@ -29,6 +29,9 @@ main = do
     handleChap04Out = either handleErr (mapM_ (putStrLn . prettyPrintToken))
     handleChap06Out = either handleErr (putStrLn . prettyPrintExpr)
     handleChap07Out = either handleErr (putStrLn . printValue)
+
+currentImpl :: String -> Either InterpreterError Value
+currentImpl = runChapter04 >=> runChapter06 >=> runChapter07
 
 -- Chapter 04 operations
 runChapter04 :: String -> Either InterpreterError [Token]

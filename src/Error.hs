e9b8219794be893d186cadee51631a0ee95f2f7b
David Sánchez <davidslt+git@pm.me>
2025-09-24 22:47:32 +0100

    style: remove indication of error type
    CraftInt tests do not expect them

--- src/Error.hs
+++ src/Error.hs
@@ -9,14 +9,11 @@ data InterpreterError = Syntax [SyntaxError] | Parse String | Eval String derivi
 handleErr :: InterpreterError -> IO ()
 handleErr = \case
   Syntax errs -> do
-    putStrLn "Syntax errors found:"
     mapM_ (putStrLn . prettyPrintErr) errs
     exitWith (ExitFailure 65)
   Parse err -> do
-    putStrLn "Parsing error:"
     putStrLn err
     exitWith (ExitFailure 65)
   Eval err -> do
-    putStrLn "Evaluation error:"
     putStrLn err
     exitWith (ExitFailure 70)

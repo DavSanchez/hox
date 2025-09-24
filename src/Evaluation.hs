195b1d7548e0e4109b666336db8c993cc6382ac9
David Sánchez <davidslt+git@pm.me>
2025-09-24 22:48:01 +0100

    style: correct error messages
    Done the way CraftInt tests expect them

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -42,29 +42,40 @@ evalExpr (Binary op e1 e2) = do
 
 evalUnaryOp :: UnaryOperator -> Value -> Either String Value
 evalUnaryOp UMinus (VNumber n) = Right $ VNumber (negate n)
+evalUnaryOp UMinus _ = Left "Operand must be a number."
 evalUnaryOp Bang v = (Right . VBool . not . isTruthy) v
-evalUnaryOp op v = Left $ "Invalid unary operation: " <> show op <> " with " <> show v
 
 evalBinaryOp :: BinaryOperator -> Value -> Value -> Either String Value
--- Some numeric operations
+-- Greater than
 evalBinaryOp Greater (VNumber n1) (VNumber n2) = Right $ VBool (n1 > n2)
+evalBinaryOp Greater _ _ = Left "Operands must be numbers."
+-- Greater than or equal to
 evalBinaryOp GreaterEqual (VNumber n1) (VNumber n2) = Right $ VBool (n1 >= n2)
+evalBinaryOp GreaterEqual _ _ = Left "Operands must be numbers."
+-- Less than
 evalBinaryOp Less (VNumber n1) (VNumber n2) = Right $ VBool (n1 < n2)
+evalBinaryOp Less _ _ = Left "Operands must be numbers."
+-- Less than or equal to
 evalBinaryOp LessEqual (VNumber n1) (VNumber n2) = Right $ VBool (n1 <= n2)
+evalBinaryOp LessEqual _ _ = Left "Operands must be numbers."
+-- Subtraction only works for two numbers
 evalBinaryOp BMinus (VNumber n1) (VNumber n2) = Right $ VNumber (n1 - n2)
+evalBinaryOp BMinus _ _ = Left "Operands must be numbers."
+-- Summation only works for two numbers (sum) or two strings (concatenation)
 evalBinaryOp Plus (VNumber n1) (VNumber n2) = Right $ VNumber (n1 + n2)
--- Concatenation for strings
 evalBinaryOp Plus (VString s1) (VString s2) = Right $ VString (s1 ++ s2)
--- More numerics: Division and multiplication, with error handling for division by zero
+evalBinaryOp Plus _ _ = Left "Operands must be two numbers or two strings."
+-- Division and multiplication, with error handling for division by zero
 evalBinaryOp Slash (VNumber n1) (VNumber n2)
   | n2 == 0 = Left "Division by zero" -- Handle or just fail?
   | otherwise = Right $ VNumber (n1 / n2)
+evalBinaryOp Slash _ _ = Left "Operands must be numbers."
+-- Multiplication
 evalBinaryOp Star (VNumber n1) (VNumber n2) = Right $ VNumber (n1 * n2)
+evalBinaryOp Star _ _ = Left "Operands must be numbers."
 -- Equality and inequality checks
 evalBinaryOp EqualEqual v1 v2 = Right $ VBool (v1 == v2)
 evalBinaryOp BangEqual v1 v2 = Right $ VBool (v1 /= v2)
--- Unsupported operations, or mismatched types
-evalBinaryOp op v1 v2 = Left $ "Invalid binary operation: " <> show op <> " with " <> show v1 <> " and " <> show v2
 
 evalLiteral :: Literal -> Value
 evalLiteral (Number n) = VNumber n

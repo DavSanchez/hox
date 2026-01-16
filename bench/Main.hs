f4598e01cb72099f31607c8fe81771dea071f943
David Sánchez <davidslt+git@pm.me>
2026-01-16 21:37:54 +0000

    perf: configurable fib benches

--- bench/Main.hs
+++ bench/Main.hs
@@ -7,14 +7,14 @@ import Language.Syntax.Token (Token)
 import Runtime.Interpreter (buildTreeWalkInterpreter, runInterpreter)
 import Test.Tasty.Bench
 
-fibSrc :: String
-fibSrc =
+fibSrc :: Int -> String
+fibSrc n =
   unlines
     [ "fun fib(n) {",
       "  if (n < 2) return n;",
       "  return fib(n - 1) + fib(n - 2);",
       "}",
-      "var f = fib(20);"
+      "var f = fib(" ++ show n ++ ");"
     ]
 
 -- Helper to extract tokens (assuming valid input for benchmark)
@@ -28,24 +28,28 @@ extractTokens (r :| rs) = rights (r : rs)
 main :: IO ()
 main = do
   -- Pre-compute values to avoid benchmarking setup costs in the loops
-  let tokens = extractTokens $ scanTokens fibSrc
+  let tokens n = extractTokens $ scanTokens (fibSrc n)
   -- Note: We are benchmarking the parsing of the *token list*, not the string.
 
   defaultMain
     [ bgroup
         "Scanner"
         -- Force evaluation by calculating length of the list
-        [ bench "fib(20)" $ whnf (length . scanTokens) fibSrc
+        [ bench "fib(20)" $ whnf (length . scanTokens) (fibSrc 20)
+        -- additional tests should be done with bigger files
         ],
       bgroup
         "Parser"
         -- Force evaluation by checking if it's a Right
-        [ bench "fib(20)" $ whnf (isRight . parseProgram) tokens
+        [ bench "fib(20)" $ whnf (isRight . parseProgram) (tokens 20)
+        -- additional tests should be done with bigger files
         ],
       bgroup
         "Interpreter"
         -- whnfIO is sufficient because the return value is simple key to success
-        [ bench "fib(20)" $ whnfIO (runInterpreter (buildTreeWalkInterpreter (Right tokens)))
+        [ bench "fib(20)" $ whnfIO (runInterpreter (buildTreeWalkInterpreter (Right (tokens 20)))),
+          bench "fib(25)" $ whnfIO (runInterpreter (buildTreeWalkInterpreter (Right (tokens 25)))),
+          bench "fib(30)" $ whnfIO (runInterpreter (buildTreeWalkInterpreter (Right (tokens 30))))
         ]
     ]
 
